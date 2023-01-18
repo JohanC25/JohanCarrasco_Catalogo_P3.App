1bd5ca22fb1e38440ca2700076e34e89e20872f2
Johan Carrasco <johanmcm@hotmail.com>
2023-01-18 07:08:48 -0500

    Add project files.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace JohanCarrasco_Catalogo_P3;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
